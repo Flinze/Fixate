7b9b8f84333a28a1947c77c824b946eaf29ea90e
j-omi <gomijoy@gmail.com>
2019-01-26 20:47:44 -0800

    adding functions for cancel button

--- app/src/main/java/com/fixate/Task.java
+++ app/src/main/java/com/fixate/Task.java
@@ -4,29 +4,38 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class Task extends AppCompatActivity {
+public class Task extends AppCompatActivity implements View.OnClickListener {
 
     private TextView countDownText = null;
     private int currTask;
+    private long currMinutes = 0;
+    private ImageButton cancelButton;
+    private ImageButton pauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
         countDownText = (TextView) findViewById(R.id.countdownText);
+        cancelButton = findViewById(R.id.cancelButton);
+        pauseButton = findViewById(R.id.pauseButton);
+
         currTask = getIntent().getIntExtra("currTask", 0);
         createTimeCountDown(25);
     }
 
-    protected void createTimeCountDown(int time) {
+    private void createTimeCountDown(int time) {
         CountDownTimer timer = new CountDownTimer(time * 60000, 1000) {
             @Override
             public void onTick(long millisRemaining) {
                 long secondsRemaining = millisRemaining / 1000;
                 long minutes = secondsRemaining / 60;
                 long seconds = secondsRemaining % 60;
+                currMinutes = minutes;
                 countDownText.setText(String.format("%02d:%02d", minutes, seconds));
             }
 
@@ -45,4 +54,20 @@ public class Task extends AppCompatActivity {
         }.start();
     }
 
+    private void cancelButton() {
+        if (currMinutes <= 23) {
+            View b = findViewById(R.id.cancelButton);
+            b.setVisibility(View.GONE);
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.cancelButton:
+                Intent i = new Intent(Task.class, MainActivity.class);
+
+    }
 }
